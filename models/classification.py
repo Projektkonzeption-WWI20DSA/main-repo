164a5e12648f839f07583c93e8da956cd396de93
Chengyi Hua <Chengyi.moyi@gmail.com>
2023-07-03 00:40:02 +0200

    adding new call-up

--- models/classification.py
+++ models/classification.py
@@ -1,7 +1,12 @@
+import joblib
+
 def summarize_text(text):
+    # Load the trained model
+    pipeline_02 = joblib.load('trained_model.joblib')
+
     # Perform the necessary operations to classify the text
     # Replace this with your actual text classification logic
-    
-    classification = "Sample Category. (origianl text: " +text+ ")"  # Replace with the generated classification
-    
+    classification = pipeline_02.predict([text])[0]  # Replace with the generated classification
+
+    classification = f"Sample Category. (original text: {text})"
     return classification
