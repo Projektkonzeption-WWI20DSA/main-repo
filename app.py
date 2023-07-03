aee889fd6c0439ee0b73738ef3dbc757233b1eb0
Qusay99 <qusay.hamid9@gmail.com>
2023-07-03 23:50:24 +0200

    enabled uploading txt and docx files

--- app.py
+++ app.py
@@ -1,58 +1,55 @@
-from flask import Flask, render_template, request, jsonify
-from models import summary, classification  # Import the Python files
+from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
-from io import TextIOWrapper
+import docx2txt
+import os
+from models import summary, classification
 
 app = Flask(__name__)
-app.config["UPLOAD_FOLDER"] = "static/"
+app.config["UPLOAD_FOLDER"] = "uploads"
+app.config["ALLOWED_EXTENSIONS"] = ["txt", "docx"]
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config["ALLOWED_EXTENSIONS"]
 
-# @app.route('/process-text', methods=['POST'])
-# def process_text():
-#     data = request.get_json()
-#     text = data['text']
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    processed_text = None
+    summary_result = None
+    classification_result = None
+    content = ''
 
-#     # Return the response
-#     response = {'message': 'Text received and processed successfully'}
-#     return jsonify(response)
-
-@app.route('/process-summary', methods=['POST'])
-def process_summary():
-    data = request.get_json()
-    text = data['text']
-    
-    # Process the summary using the 'summary.py' module
-    summary_result = summary.process_summary(text)
-    
-    # Return the summary response
-    response = {'summary': summary_result}
-    return jsonify(response)
-
-@app.route('/process-classification', methods=['POST'])
-def process_classification():
-    data = request.get_json()
-    text = data['text']
-    
-    # Process the classification using the 'classification.py' module
-    classification_result = classification.process_classification(text)
-    
-    # Return the classification response
-    response = {'classification': classification_result}
-    return jsonify(response)
-
-# Uploading files
-
-@app.route('/display', methods = ['GET', 'POST'])
-def save_file():
     if request.method == 'POST':
-        f = request.files['file']
-        print("file saved successfully")
-        content = f.read().decode('utf-8')
-        print("Content:\n", content) 
+        text = request.form.get('input-text')
+        file = request.files.get('file')
         
+
+        if file and allowed_file(file.filename):
+            if file.filename.endswith('.docx'):
+                text = ''  # Leeren String initialisieren
+                text += docx2txt.process(file)
+                content = text
+            elif file.filename.endswith('.txt'):
+                text = ''  # Leeren String initialisieren
+                text += file.read().decode('utf-8')
+                content = text
+
+            # Setzen Sie das Textfeld auf den Inhalt der hochgeladenen Datei
+            # request.form['input-text'] = text
+
+        if text:
+            if request.form.get('summary'):
+                print("Summary Selected")
+                # summary_result = summary.process_summary(text)
+                processed_text = text
+
+            if request.form.get('classification'):
+                print("Classification Selected")
+                # classification_result = classification.process_classification(text)
+                processed_text = text
+
+            # processed_text = text
+
+    # return render_template('index.html', content=content,processed_text=processed_text, summary_result=summary_result, classification_result=classification_result)
     return render_template('index.html', content=content) 
 
 if __name__ == '__main__':
