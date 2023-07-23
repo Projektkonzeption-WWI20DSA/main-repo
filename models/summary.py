29cf7e279c3bbe36a87eb378ea0300eae05a2595
Felix S <105482809+FelixSHA@users.noreply.github.com>
2023-07-23 13:24:58 +0200

    updated model scripts to chengyis latest state

--- models/summary.py
+++ models/summary.py
@@ -1,7 +1,7 @@
-def classify_text(text):
+def summerize_text(text):
     # Perform the necessary operations to generate the summary
     # Replace this with your actual summary generation logic
     
     summary = "This is a sample summary. (original text: " +text+")"  # Replace with the generated summary
     
-    return summary
+    return summarya
